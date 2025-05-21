cfaa815ecf72f53e36ac72cf098da1bbc4fab153
laziw-001 <abduqodirovalaziza866@gmail.com>
2025-05-22 00:25:21 +0500

    Update package files and vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/react/',
+  base: "/react/", // <<< bu MUHIM! repo noming bilan bir xil bo‘lishi kerak
   plugins: [react()],
 })
